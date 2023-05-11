96a4c5c59a54119a4959ede12bb46bfc8b95e8d1
GabrielF <bakerff77@gmail.com>
2023-05-11 16:23:02 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // o código de cima faz sozinho essa função toda aq
+
+ // if (html.classList.contains('light')) {
+ //  html.classList.remove('light')
+ // } else {
+ // html.classList.add('light')
+ // }
+  
+
+   // AGORA A TROCA DE IMAGEM!
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver sem, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
